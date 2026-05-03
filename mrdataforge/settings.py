9022bd4fd2a2974fdcf4400b0ef3240e1f14b118
pooja <poojadamewar321@gmail.com>
2026-05-03 14:40:20 +0530

    dfg

--- mrdataforge/settings.py
+++ mrdataforge/settings.py
@@ -131,3 +131,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+# WhiteNoise configuration for serving static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
