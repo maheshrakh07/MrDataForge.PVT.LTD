4e68265a536308d6fc04c67b0c2a928b0328bc98
pooja <poojadamewar321@gmail.com>
2026-05-03 13:49:44 +0530

    bv

--- apanel/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApanelConfig(AppConfig):
-    name = 'apanel'
--- spanel/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpanelConfig(AppConfig):
-    name = 'spanel'
