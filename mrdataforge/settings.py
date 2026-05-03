875aa58dca0f88bed8c49b12b529845ebb513c96
pooja <poojadamewar321@gmail.com>
2026-05-03 12:21:47 +0530

    update setting

--- mrdataforge/settings.py
+++ mrdataforge/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-m=4@^dkoy_fnds4!&o9-x$z6!rs&0uf)9^(d#)-j*vpjouc627
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = [
-    'mrdataforge-pvt-ltd-2.onrender.com'
+    "mrdataforge-pvt-ltd-2.onrender.com",
+    "127.0.0.1",
+    "localhost",
 ]
 # Application definition
 
