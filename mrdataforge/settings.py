556da2f9b09dab636ad9772fcb24d7ebcad793a2
pooja <poojadamewar321@gmail.com>
2026-05-16 13:43:04 +0530

    xvnd

--- mrdataforge/settings.py
+++ mrdataforge/settings.py
@@ -132,3 +132,11 @@ STATICFILES_DIRS = [
 ]
 # WhiteNoise configuration for serving static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import os
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
